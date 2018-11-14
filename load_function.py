c608109a846b615af038d3c413e64af10f0cf3e7
Max Heikenfeld <maxheikenfeld@web.de>
2018-11-14 23:43:27 +0000

    added loading for detailed luping of processes and hydrometeor mixing ratios

--- load_function.py
+++ load_function.py
@@ -1,12 +1,12 @@
 def set_load_function(model):
     import ramscube
     import wrfcube
-    from wrfmpdiag import calculate_wrf_mp_path,calculate_rams_mp_path,lump_processes
+    import iris
+    from mpdiag import calculate_wrf_mp_path,calculate_rams_mp_path,lump_processes
     #Define load functions for the two models and place them in dictionary
     #General load function    
     if model=='WRF':
         load_function=wrfcube.load
-        
         def load_function_out(filename,variable_in):
             if variable_in is 'processes':
                 processes=calculate_wrf_mp_path(filename,                                                    
@@ -23,7 +23,18 @@ def set_load_function(model):
                                           debug_nproc=None)
                 processes_lumped=lump_processes(processes,microphysics_scheme='morrison',lumping='basic')
                 out=processes_lumped
-
+            elif variable_in is 'processes_lumped_detailed':
+                processes=calculate_wrf_mp_path(filename,                                                    
+                                          microphysics_scheme='morrison',processes='mass',
+                                          signed=True,
+                                          quantity='mixing ratio',constraint=None,
+                                          debug_nproc=None)
+                processes_lumped=lump_processes(processes,microphysics_scheme='morrison',lumping='detailed')
+                out=processes_lumped
+            elif variable_in is "hydrometeors":
+                out=iris.cube.Cubelist()
+                for hydrometeor in ['QCLOUD','QRAIN','QICE','QSNOW','QGRAUP']:
+                    out.append(load_function(filename,hydrometeor))
             else:
                 if variable_in=='w':
                     variable='W'
@@ -51,8 +62,14 @@ def set_load_function(model):
                                                  signed=True,
                                                  quantity='mixing ratio',constraint=None,
                                                  debug_nproc=None)
+                # if process rates are accumulated, divide by timestep (deemd ok for 1min data)
+                time_coord=processes[0].coord('time')
+                dt=(time_coord.units.num2date(time_coord.points[1])-time_coord.units.num2date(time_coord.points[0])).total_seconds()
+                dt_coord=iris.coords.AuxCoord(dt,units='s')
+                for i,process in enumerate(processes):
+                    processes[i]=process/dt_coord
+                    processes[i].rename(process.name())
                 out=processes
-
             elif variable_in is 'processes_lumped':
                 processes=calculate_rams_mp_path(filename,                                                    
                                                  microphysics_scheme='RAMS',processes='mass',
@@ -60,8 +77,36 @@ def set_load_function(model):
                                                  quantity='mixing ratio',constraint=None,
                                                  debug_nproc=None)
                 processes_lumped=lump_processes(processes,microphysics_scheme='RAMS',lumping='basic')
+                
+                # if process rates are accumulated, divide by timestep (deemd ok for 1min data)
+                time_coord=processes_lumped[0].coord('time')
+                dt=(time_coord.units.num2date(time_coord.points[1])-time_coord.units.num2date(time_coord.points[0])).total_seconds()
+                dt_coord=iris.coords.AuxCoord(dt,units='s')
+                for i,process in enumerate(processes_lumped):
+                    processes_lumped[i]=process/dt_coord
+                    processes_lumped[i].rename(process.name())
                 out=processes_lumped
-
+                
+            elif variable_in is 'processes_lumped_detailed':
+                processes=calculate_rams_mp_path(filename,                                                    
+                                                 microphysics_scheme='RAMS',processes='mass',
+                                                 signed=True,
+                                                 quantity='mixing ratio',constraint=None,
+                                                 debug_nproc=None)
+                processes_lumped=lump_processes(processes,microphysics_scheme='RAMS',lumping='detailed')
+                
+                # if process rates are accumulated, divide by timestep (deemd ok for 1min data)
+                time_coord=processes_lumped[0].coord('time')
+                dt=(time_coord.units.num2date(time_coord.points[1])-time_coord.units.num2date(time_coord.points[0])).total_seconds()
+                dt_coord=iris.coords.AuxCoord(dt,units='s')
+                for i,process in enumerate(processes_lumped):
+                    processes_lumped[i]=process/dt_coord
+                    processes_lumped[i].rename(process.name())
+                out=processes_lumped
+            elif variable_in is "hydrometeors":
+                out=iris.cube.Cubelist()
+                for hydrometeor in ['RCP','RDP','RRP','RPP','RSP','RAP','RGP','RHP']:
+                    out.append(load_function(filename,hydrometeor))
             else:
                 if variable_in=='w':
                     variable='WC'
